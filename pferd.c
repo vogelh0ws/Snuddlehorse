47b3838f57f20526adc3d006e8ffb58b91a39062
Anselm <newsundstuff@gmx.de>
2012-06-03 15:48:54 +0200

    feed the horses

--- pferd.c
+++ pferd.c
@@ -1,6 +1,9 @@
+
 #include <SDL/SDL.h> 
 #include <stdlib.h>
 #include <stdio.h>
+
+#define DEBUG
 
 #define MEADOW_COLOR 0x339933
 
@@ -21,12 +24,18 @@ typedef struct
 {
 	int		xpos;
 	int		ypos;
-//	int		xdest;
-//	int		ydest;
-//	Direction	direct;
+	int		dist;
+	Direction	direct;
 	SDL_Surface*	pSurfRight;
 	SDL_Surface*	pSurfLeft;
 } Pferd;
+
+typedef struct
+{
+	int	 	xpos;
+	int		ypos;
+	SDL_Surface* 	pSurf;
+} Food;
 
 SDL_Surface* m_screen;
 SDL_Surface* m_background;
@@ -47,7 +56,6 @@ void put_pixel32( SDL_Surface *surface, int x, int y, int pixel )
 
 SDL_Surface *flip_surface( SDL_Surface *surface, int flags )
 {
-	//TODO copying colorkey does not work
 	int pixel;
 	SDL_Surface *flipped;
 
@@ -90,7 +98,7 @@ SDL_Surface *flip_surface( SDL_Surface *surface, int flags )
 	{
 		SDL_UnlockSurface( surface );
 	}
-	
+
 	if( surface->flags & SDL_SRCCOLORKEY )
 	{
 		SDL_SetColorKey( flipped, SDL_RLEACCEL | SDL_SRCCOLORKEY, surface->format->colorkey );
@@ -106,30 +114,59 @@ void dumpSurface( SDL_Surface* pSurf )
 	printf("		format->Ashift:	 0x%X\n", pSurf->format->Ashift);
 }
 
+Food* dropFood( int xpos, int ypos )
+{
+	int colorkey;
+	char* bmpFilename = "carrot.bmp";
+
+	Food* pFood = malloc( sizeof(Food) );
+
+	pFood->xpos = xpos;
+	pFood->ypos = ypos;
+
+	pFood->pSurf = SDL_DisplayFormatAlpha( SDL_LoadBMP( bmpFilename ) );
+
+	colorkey = SDL_MapRGB( pFood->pSurf->format, 0xFF, 0x00, 0x00 );
+	SDL_SetColorKey( pFood->pSurf, SDL_RLEACCEL | SDL_SRCCOLORKEY, colorkey );
+
+	pFood->pSurf->format->Amask  = 0; 
+
+	return pFood;
+}
+
+void eatFood( Food* pFood )
+{
+	free( pFood );
+}
+
 Pferd* createPferd( int xpos, int ypos, char* bmpFilename )
 {
 	int colorkey;
 
 	Pferd* pNewPferd = malloc( sizeof(Pferd) );
 
-	pNewPferd->xpos = xpos;
-	pNewPferd->ypos = ypos;
-	
+	pNewPferd->xpos   = xpos;
+	pNewPferd->ypos   = ypos;
+	pNewPferd->dist   = 0;
+	pNewPferd->direct = left;
+
 	pNewPferd->pSurfRight = SDL_DisplayFormatAlpha( SDL_LoadBMP ( bmpFilename ) );
 
 	colorkey = SDL_MapRGB( pNewPferd->pSurfRight->format, 0xFF, 0x00, 0x00 );
 	SDL_SetColorKey( pNewPferd->pSurfRight, SDL_RLEACCEL | SDL_SRCCOLORKEY, colorkey );
-	
+
 	pNewPferd->pSurfLeft = flip_surface( pNewPferd->pSurfRight, FLIP_HORIZONTAL|SDL_SRCCOLORKEY );
 
 	pNewPferd->pSurfRight->format->Amask  = pNewPferd->pSurfLeft->format->Amask; 
 	//pNewPferd->pSurfRight->format->Ashift = pNewPferd->pSurfLeft->format->Ashift; 
 
+#ifdef DEBUG
 	printf("SurfRight: \n");
 	dumpSurface( pNewPferd->pSurfRight );
-	
+
 	printf("\nSurfLeft: \n");
 	dumpSurface( pNewPferd->pSurfLeft );
+#endif
 
 	return pNewPferd;
 }
@@ -139,17 +176,26 @@ void destroyPferd( Pferd* pPferd )
 	free( pPferd );
 }
 
-void movePferd( Pferd* pPferd, Direction direct, int dist )
+void goPferd( Pferd* pPferd, Direction direct, int dist )
 {
-	SDL_Rect pferdPos;
-	SDL_Rect zeroRect;
+#ifdef DEBUG
+	printf("	goPferd( 0x%X, %i, %i )\n", (int) pPferd, direct, dist);
+#endif
 
-	zeroRect.x = 0;
-	zeroRect.y = 0;
+	pPferd->dist   = dist;
+	pPferd->direct = direct;
+}
 
-	while ( dist > 0 )
+void movePferd( Pferd* pPferd )
+{
+#ifdef DEBUG
+	printf("	movePferd( 0x%X )\n", (int) pPferd);
+	printf("		pPferd->dist: %i\n", pPferd->dist);
+#endif
+
+	if ( pPferd->dist > 0 )
 	{
-		switch (direct) 
+		switch (pPferd->direct) 
 		{
 			case left:
 				pPferd->xpos -= 20;
@@ -174,117 +220,240 @@ void movePferd( Pferd* pPferd, Direction direct, int dist )
 				pPferd->xpos -= 10;
 				break;
 		}
-		dist -= 20;
-		
-		pferdPos.x = pPferd->xpos;
-		pferdPos.y = pPferd->ypos;
+		pPferd->dist -= 20;
 
-		// SDL_FillRect( m_screen, &m_screen->clip_rect, MEADOW_COLOR );
-		SDL_BlitSurface( m_background, NULL, m_screen, &zeroRect );
-		
-		switch (direct)
-		{
-			case left:
-			case upleft:
-			case downleft:
-				SDL_BlitSurface( pPferd->pSurfLeft, NULL, m_screen, &pferdPos );
-				break;
-			case right:
-			case upright:
-			case downright:
-				SDL_BlitSurface( pPferd->pSurfRight, NULL, m_screen, &pferdPos );
-				break;
-			default:
-				SDL_BlitSurface( pPferd->pSurfLeft, NULL, m_screen, &pferdPos );
-		}
-		
-		SDL_Flip( m_screen );
-		SDL_Delay( 250 );
 	}
-	
+
 }
 
-void wanderPferd( Pferd* pPferd, int times )
+void wanderPferd( Pferd* pPferd )
 {
 	int distance;
 
-	while (times)
+	distance = ( rand() % 500 );
+#ifdef DEBUG
+	printf("	wanderPferd( 0x%X )\n", (int) pPferd);
+	printf("		distance: %i\n", distance);
+#endif
+	if ( ((pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w ) &&
+			((pPferd->ypos + distance + pPferd->pSurfRight->h) < m_screen->h ) )
 	{
-		distance = ( rand() % 500 );
-		if ( ((pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w ) &&
-				((pPferd->ypos + distance + pPferd->pSurfRight->h) < m_screen->h ) )
-		{
-			movePferd( pPferd, downright, distance );
-			times--;
-		}
-		else if( ((pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w ) &&
-				((pPferd->ypos - distance ) > 0 ) )
-		{
-			movePferd( pPferd, upright, distance );
-			times--;
-		}
-		else if( ((pPferd->xpos - distance) > 0 ) &&
-				((pPferd->ypos + distance + pPferd->pSurfRight->h) < m_screen->h ) )
-		{
-			movePferd( pPferd, downleft, distance );
-			times--;
-		}
-		else if( ((pPferd->xpos - distance) > 0 ) &&
-				((pPferd->ypos - distance ) > 0 ) )
-		{
-			movePferd( pPferd, upleft, distance );
-			times--;
-		}
-		else if ( (pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w )
-		{
-			movePferd( pPferd, right, distance );
-			times--;
-		}  
-		else if ( (pPferd->xpos - distance) > 0 )
-		{
-			movePferd( pPferd, left, distance );
-			times--;
-		}
-	}	
+		goPferd( pPferd, downright, distance );
+	}
+	else if( ((pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w ) &&
+			((pPferd->ypos - distance ) > 0 ) )
+	{
+		goPferd( pPferd, upright, distance );
+	}
+	else if( ((pPferd->xpos - distance) > 0 ) &&
+			((pPferd->ypos + distance + pPferd->pSurfRight->h) < m_screen->h ) )
+	{
+		goPferd( pPferd, downleft, distance );
+	}
+	else if( ((pPferd->xpos - distance) > 0 ) &&
+			((pPferd->ypos - distance ) > 0 ) )
+	{
+		goPferd( pPferd, upleft, distance );
+	}
+	else if ( (pPferd->xpos + distance + pPferd->pSurfRight->w) < m_screen->w )
+	{
+		goPferd( pPferd, right, distance );
+	}  
+	else if ( (pPferd->xpos - distance) > 0 )
+	{
+		goPferd( pPferd, left, distance );
+	}
+}	
+
+void blitPferd( Pferd* pPferd ) 
+{
+	SDL_Rect pferdPos;
+
+	pferdPos.x = pPferd->xpos;
+	pferdPos.y = pPferd->ypos;
+
+	switch (pPferd->direct)
+	{
+		case left:
+		case upleft:
+		case downleft:
+			SDL_BlitSurface( pPferd->pSurfLeft, NULL, m_screen, &pferdPos );
+			break;
+		case right:
+		case upright:
+		case downright:
+			SDL_BlitSurface( pPferd->pSurfRight, NULL, m_screen, &pferdPos );
+			break;
+		default:
+			SDL_BlitSurface( pPferd->pSurfLeft, NULL, m_screen, &pferdPos );
+	}
+
+}
+
+void blitFood( Food* pFood )
+{
+	SDL_Rect foodPos;
+
+	foodPos.x = pFood->xpos;
+	foodPos.y = pFood->ypos;
+
+	SDL_BlitSurface( pFood->pSurf, NULL, m_screen, &foodPos );
+}
+
+void getFood( Pferd* pPferd, Food* pFood )
+{
+	Direction direct;
+	int 	  dist;
+
+#ifdef DEBUG
+	printf("	getFood( 0x%X, 0x%X )\n", (int) pPferd, (int) pFood);
+#endif
+
+	if ( pPferd->ypos > pFood->ypos )
+	{
+		if ( pPferd->xpos > pFood->xpos )
+			direct = upleft;
+		else
+			direct = upright;
+
+		dist   = pPferd->ypos - pFood->ypos;
+	}
+	else if ( pPferd->ypos < pFood->ypos )
+	{
+		if ( pPferd->xpos > pFood->xpos )
+			direct = downleft;
+		else
+			direct = downright;
+
+		dist   = pFood->ypos - pPferd->ypos;
+	}
+	else if ( pPferd->xpos > pFood->xpos )
+	{
+		direct = left;
+		dist   = pPferd->xpos - pFood->xpos;
+	}
+	else if (pPferd->xpos < pFood->xpos )
+	{
+		direct = right;
+		dist   = pFood->xpos - pPferd->xpos;
+	}
+
+
+	goPferd( pPferd, direct, dist );
+}
+
+int tryEat( Pferd* pPferd, Food* pFood )
+{
+	if ( (pPferd->xpos == pFood->xpos) && (pPferd->ypos == pFood->ypos) )
+		return 1;
+	else
+		return 0;
 }
 
 int main( int argc, char* args[] ) 
 { 
+	int 	  fExit 	 = 0;
+	int	  fFoodAvailable = 0;
+	int	  xfood, yfood;
+
 	SDL_Event event;
-	Pferd* 	  pSPferd;
+	SDL_Rect  zeroRect;
+
+	Pferd* 	  pSPferd; 
+	Pferd*	  pLea;
+	Food* 	  pFood;
+
+	zeroRect.x = 0;
+	zeroRect.y = 0;
 
 	//Start SDL 
 	SDL_Init( SDL_INIT_EVERYTHING ); 
-	
+
 	//atexit(SDL_Quit);
 
 	SDL_WM_SetCaption("Snuddlehorse", "Snuddlehorse");
 
 	m_screen = SDL_SetVideoMode( 800, 600, 32, SDL_SWSURFACE );
 	m_background = SDL_LoadBMP( "background.bmp" );
-	
 
-
+	pLea	= createPferd( 600, 100, "lea.bmp" );
 	pSPferd = createPferd( 400, 300, "pferd.bmp" );
 
-	while (1)
+	while ( !fExit )
 	{
-		wanderPferd( pSPferd, 1 );
+		if ( pSPferd->dist <= 0 )
+		{
+			if ( fFoodAvailable )
+				getFood( pSPferd, pFood );
+			else
+				wanderPferd( pSPferd );
+		}
+		if ( pLea->dist <= 0 )
+		{
+			if ( fFoodAvailable )
+				getFood( pLea, pFood );
+			else
+				wanderPferd( pLea );
+		}
+
+		if ( fFoodAvailable )
+		{
+			if ( tryEat( pSPferd, pFood ) || tryEat( pLea, pFood ) )
+			{
+				eatFood( pFood );
+				fFoodAvailable = 0;
+			}
+		}
+
+		// SDL_FillRect( m_screen, &m_screen->clip_rect, MEADOW_COLOR );
+		SDL_BlitSurface( m_background, NULL, m_screen, &zeroRect );
+
+		if ( fFoodAvailable )
+			blitFood( pFood );
+
+		movePferd( pLea );
+		movePferd( pSPferd );
+
+		blitPferd( pLea );
+		blitPferd( pSPferd );
+
+		SDL_Flip( m_screen );
 
 		while(SDL_PollEvent(&event)) 
 		{
-			if (event.type == SDL_QUIT) {
-				SDL_Quit();
-				return 0;
+			switch (event.type)
+			{
+				case SDL_MOUSEBUTTONDOWN:
+					xfood = event.button.x - (event.button.x % 20);
+					yfood = event.button.y - (event.button.y % 20);
+#ifdef DEBUG
+					printf("placing food at (%d,%d)\n", xfood, yfood);
+#endif
+					if ( !fFoodAvailable ) 
+					{
+						pFood = dropFood( xfood, yfood );
+						fFoodAvailable = 1;
+					}
+					break;
+				case SDL_KEYDOWN:
+					if ( event.key.keysym.sym == SDLK_ESCAPE )
+					{
+						fExit = 1;
+					}
+					break;
+				case SDL_QUIT:
+					fExit = 1;
+					break;
 			}
 		}
+
+		SDL_Delay( 200 );
 	}
 
-	SDL_Delay( 2000 );
+	printf("quit\n");
 
-	//Quit SDL 
 	SDL_Quit(); 
-	
+
 	return 0; 
 }
 
